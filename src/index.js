e143ada7b62f617e448cd0d668df703370a9565f
TeeYuhh14 <tiademas@gmail.com>
2024-05-27 20:21:09 +0200

    Added footer

--- src/index.js
+++ src/index.js
@@ -10,5 +10,35 @@ root.render(
   <StrictMode class="Search">
     <h1>Weather App</h1>
     <SearchEngine />
+    <br />
+    <footer>
+      This project was coded by
+      <a
+        href="https://github.com/TeeYuhh14"
+        class="myProfileLink"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <strong> Tia Demas </strong>
+      </a>
+      and is on
+      <a
+        href="https://github.com/TeeYuhh14?tab=repositories"
+        class="myGitHubLink"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <strong> GitHub </strong>
+      </a>
+      and hosted on
+      <a
+        href="https://fascinating-kitten-a8f8ca.netlify.app/"
+        class="myNetlifyLink"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <strong> Netlify </strong>
+      </a>
+    </footer>
   </StrictMode>
 );
